ec5e47f0ba13afceda28c649d046504c206fd751
MercyFLesh <fleshmercy@gmail.com>
2022-11-13 03:37:16 +0300

    Update lab3

--- lab_3/LinerSystemParallel.hpp
+++ lab_3/LinerSystemParallel.hpp
@@ -1,47 +1,54 @@
 #pragma once
+#include <iostream>
 #include <vector>
+#include <mpi.h>
+
 
 namespace LinerSystem {
-    template<typename T>
-	static std::vector<double> Sor(
+	template<typename T>
+	std::vector<double> ParallelSor(
 		std::vector<std::vector<T>>& a, std::vector<T>& b, 
 		double w, int iter_limit)
 	{
 		double e = 1.0e-10;
 		std::size_t n = a.size();
 		std::vector<double> x(n, 0), res(n, 0);
-        
+		
 		for (int it = 0; it < iter_limit; it++)
 		{
 			double err = 0;
 
 			for (std::size_t i = 0; i < n; i++)
 			{
-				double sum = 0;
+				double procSum = 0;
 				for (std::size_t j = 0; j < n; j++)
 				{
-					sum -= a[i][j] * x[j];
+					procSum -= a[i][j] * x[j];
 				}
 
-				sum = w * (sum + b[i]) / a[i][i];
-				x[i] += sum;
+				double totalSum;
+				MPI_Reduce(&procSum, &totalSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+				x[i] += w * (totalSum + b[i]) / a[i][i];
 			}
 
 			for (std::size_t i = 0; i < n; i++)
 			{
-				double sum = 0;
+				double procSum = 0;
 				for (std::size_t j = 0; j < n; j++)
 				{
-					sum += a[i][j] * x[j];
+					procSum += a[i][j] * x[j];
 				}
-
-				res[i] = b[i] - sum;
+				
+				double totalSum;
+				MPI_Reduce(&procSum, &totalSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+				res[i] = b[i] - totalSum;
 				if (std::abs(res[i - 1]) > err) 
 				{
  					err = std::abs(res[i - 1]);
  				}
 			}
 
+			//MPI_Bcast(&err, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 			if (err <= e)
  				break;
 		}
--- lab_3/main.cxx
+++ lab_3/main.cxx
@@ -1,19 +1,112 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include <tuple>
+#include <chrono>
+#include <cassert>
+#include <mpi.h>
 #include "LinerSystem.hpp"
+#include "LinerSystemParallel.hpp"
 
 using namespace std;
 
-int main()
+static random_device rd;
+static uniform_real_distribution<double> urd(-10, 10);
+static const double w = 1.2;
+static const int limit_iterations = 1500;
+static vector<int> sizes {10, 100, 1000, 2000};
+
+
+tuple<vector<vector<double>>, vector<double>> initializer_matrix(int size)
 {
-    vector<vector<double>> matrix {{3, 5}, {4, 8}};
-    vector<double> b{11, 16};
+    vector<vector<double>> a(size, vector<double>(size));
+    vector<double> b(size);
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+            a[i][j] = urd(rd);
+        b[i] = urd(rd);
+    }
 
-    double w = 1.5;
-    int steps = 1000;
+    return {move(a), move(b)};
+}
 
-    vector<double> x = LinerSystem::sor(matrix, b, w, steps);
-    for(auto& n : x)
-        cout << n << " ";
+void ValidTest()
+{
+    vector<vector<int>> a{{3, 5}, {4, 8}};
+    vector<int> b{11, 16};
+
+    vector<double> x = LinerSystem::Sor(a, b, w, limit_iterations);
+
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        for (size_t j = 0; j < a[0].size(); j++)
+        {
+            cout << "(x" << j + 1 << " * " << a[i][j] << ") ";
+            if (j != a[0].size() - 1)
+                cout << "+ ";
+        }
+
+        cout << "= " << b[i] << endl;
+    }
+
+    for (size_t i = 0; i < x.size(); i++)
+        cout << "x" << i + 1 << " = " << x[i] << endl;
+}
+
+void PerfomanceSequentialTest()
+{
+    cout << "Sequential algorithm performance" << endl;
+    cout << "Matrix size\t" <<  "Time ms." << endl;
+
+    for (size_t i = 0; i < sizes.size(); i++)
+    {
+        auto [a, b] = initializer_matrix(sizes[i]);
+        auto begin = chrono::steady_clock::now();
+        LinerSystem::Sor(a, b, w, limit_iterations);
+        auto time = chrono::duration_cast<chrono::microseconds>(
+                chrono::steady_clock::now() - begin).count();
+        
+        cout << sizes[i] << "\t\t" << time << endl;
+    }
+}
+
+void PerfomanceParallelTest()
+{
+    int procesSize, procesRank;
+    MPI_Comm_size(MPI_COMM_WORLD, &procesSize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procesRank);
+
+    if (procesRank == 0)
+    {
+        cout << "Parallel algorithm performance with " << procesSize << " processes" << endl;
+        cout << "Matrix size\t" <<  "Time ms." << endl;
+    }
+
+    for (size_t i = 0; i < sizes.size(); i++)
+    {
+        auto [a, b] = initializer_matrix(sizes[i]);
+        auto begin = chrono::steady_clock::now();
+        LinerSystem::ParallelSor(a, b, w, limit_iterations);
+        auto time = chrono::duration_cast<chrono::microseconds>(
+                chrono::steady_clock::now() - begin).count();
+
+        if (procesRank == 0)
+        {
+            cout << sizes[i] << "\t\t" << time << endl;
+        }
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    //ValidTest();
+    //PerfomanceSequentialTest();
+
+    MPI_Init(&argc, &argv);
     
-    cout << endl;
+    PerfomanceParallelTest();
+
+    MPI_Finalize();
 }
